a5229f8b42f8c3c95ccaca42054cef52698b90e2
Osvaldo Calombe <osvaldozamba@gmail.com>
2026-04-29 22:51:45 +0100

    Feat: CRUD ToDo Entity

---
+++ AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AuthAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
